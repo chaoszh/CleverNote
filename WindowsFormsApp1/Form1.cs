e60c834e5376ca63c737c4acb03155535660ef36
Avada Kedavra! <1499449418@qq.com>
2019-05-23 16:46:06 +0800

    initial commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWindow();
+        }
+
+
+        #region 窗口初始化
+        /// <summary>
+        /// 程序+窗口初始化
+        /// </summary>
+        private void InitializeWindow()
+        {
+            //窗口初始化
+            this.Opacity = 0.7;
+        }
+
+        private void WindowBorder()
+        {
+
+        }
+
+        #endregion
+
+        #region 窗口拖动、大小
+        /// <summary>
+        /// FormBorderStyle.None时，支持拖动、改变窗体大小
+        /// </summary>
+        /// <param name="m"></param>
+        private const int Guying_HTLEFT = 10;
+        private const int Guying_HTRIGHT = 11;
+        private const int Guying_HTTOP = 12;
+        private const int Guying_HTTOPLEFT = 13;
+        private const int Guying_HTTOPRIGHT = 14;
+        private const int Guying_HTBOTTOM = 15;
+        private const int Guying_HTBOTTOMLEFT = 0x10;
+        private const int Guying_HTBOTTOMRIGHT = 17;
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x0084:
+                    base.WndProc(ref m);
+                    Point vPoint = new Point((int)m.LParam & 0xFFFF, (int)m.LParam >> 16 & 0xFFFF);
+                    vPoint = PointToClient(vPoint);
+                    if (vPoint.X <= 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)Guying_HTTOPLEFT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)Guying_HTBOTTOMLEFT;
+                        else
+                            m.Result = (IntPtr)Guying_HTLEFT;
+                    else if (vPoint.X >= ClientSize.Width - 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)Guying_HTTOPRIGHT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)Guying_HTBOTTOMRIGHT;
+                        else
+                            m.Result = (IntPtr)Guying_HTRIGHT;
+                    else if (vPoint.Y <= 5)
+                        m.Result = (IntPtr)Guying_HTTOP;
+                    else if (vPoint.Y >= ClientSize.Height - 5)
+                        m.Result = (IntPtr)Guying_HTBOTTOM;
+                    break;
+                case 0x0201://鼠标左键按下的消息
+                    m.Msg = 0x00A1;//更改消息为非客户区按下鼠标
+                    m.LParam = IntPtr.Zero; //默认值
+                    m.WParam = new IntPtr(2);//鼠标放在标题栏内
+                    base.WndProc(ref m);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        #endregion
+
+        #region 按下热键显示新tab
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                //按F2和Ctrl就显示Hello World  
+                case Keys.F2 | Keys.Control:
+                    Label IN = new Label();
+                    IN.Text = GetWindowMessage();
+                    IN.Location = new Point(50, 50);
+                    this.Controls.Add(IN);
+                    return false;
+            }
+            return false;
+            //return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private string GetWindowMessage()
+        {
+            string str="";
+            IDataObject iData = Clipboard.GetDataObject();
+            if (iData.GetDataPresent(DataFormats.Text))
+            {
+                str = (String)iData.GetData(DataFormats.Text);
+            }
+            return str;
+        }
+        #endregion
+    }
+}
